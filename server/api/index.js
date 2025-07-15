085407c4cbec69c8920dbb0dbfc5e0f248f995a1
Reddevil@22 <hsaif1611@gmail.com>
2025-07-16 00:43:45 +0530

    with api folder

--- server/api/index.js
+++ server/api/index.js
@@ -1,43 +1,49 @@
-// const express = require('express');
+const express = require('express');
 // const pdf = require('html-pdf');
-// const bodyParser = require('body-parser');
-// const PDFTemplate = require('../documents/index.js');
+const puppeteer = require('puppeteer');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const PDFTemplate = require('../documents/index.js');
 
-// const app = express();
+const app = express();
 
-// // CORS middleware for all responses
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   if (req.method === 'OPTIONS') {
-//     return res.sendStatus(200);
-//   }
-//   next();
-// });
+// CORS middleware for all responses
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// // POST: create PDF
-// app.post('/generate-pdf', (req, res) => {
-//   const html = PDFTemplate(req.body);
+// POST: create PDF
+app.post('/generate-pdf', async (req, res) => {
+  const html = PDFTemplate(req.body);
 
-//   pdf.create(html, {}).toBuffer((err, buffer) => {
-//     if (err) {
-//       console.error('Error generating PDF:', err);
-//       return res.status(500).send('Error generating PDF');
-//     }
+  try {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+    const page = await browser.newPage();
+    await page.setContent(html, { waitUntil: 'networkidle0' });
+    const pdfBuffer = await page.pdf({ format: 'A4' });
+    await browser.close();
 
-//     // Set headers so the browser downloads it
-//     res.set({
-//       'Content-Type': 'application/pdf',
-//       'Content-Disposition': 'attachment; filename=payslip.pdf',
-//       'Content-Length': buffer.length
-//     });
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'attachment; filename=payslip.pdf',
+      'Content-Length': pdfBuffer.length,
+    });
+    res.send(pdfBuffer);
+  } catch (error) {
+    console.error('PDF generation failed:', error);
+    res.status(500).send('Error generating PDF');
+  }
+});
 
-//     res.send(buffer);
-//   });
-// });
-
-// module.exports = app;
+module.exports = app;
