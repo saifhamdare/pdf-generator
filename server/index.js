9ffe72e06228ac77124b0c71e101390ced03977d
Reddevil@22 <hsaif1611@gmail.com>
2025-07-15 13:12:22 +0530

    Remove specific CORS origin configuration and revert to default settings

--- server/index.js
+++ server/index.js
@@ -18,13 +18,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors({
-  origin: [
-    'https://pdf-generator-frontend.netlify.app',
-    // 'https://localhost:3000',
-    'http://localhost:3000',
-  ]
-}));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // post route to create PDF
