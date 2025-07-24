7b75695bd8e709d8e55fd7f0a799a1db042970b2
Reddevil@22 <hsaif1611@gmail.com>
2025-07-24 17:48:19 +0530

    added await in pdf response

--- server/api/generate-pdf.js
+++ server/api/generate-pdf.js
@@ -14,12 +14,11 @@ console.log('Received request to generate PDF:', req.method, req.body);
     const html = PDFTemplate(req.body); // Should return full HTML string
     const file = { content: html };
     const options = { format: 'A4' };
-    const pdfBuffer = pdf.generatePdf(file, options);
+    const pdfBuffer =await pdf.generatePdf(file, options);
      res.setHeader('Content-Type', 'application/pdf');
     res.setHeader('Content-Disposition', 'attachment; filename=payslip.pdf');
     return res.status(200).send(pdfBuffer); // Send the PDF buffer as response
 
-  //  return  res.status(200).send(pdfBuffer);
   } catch (err) {
     console.error('PDF generation error:', err);
     return res.status(500).send('Failed to generate PDF');
