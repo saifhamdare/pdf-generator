294bbc3750d75d576ec6777cfa4e2b0f4731e220
Reddevil@22 <hsaif1611@gmail.com>
2025-07-26 13:38:25 +0530

    save

--- server/api/generate-pdf.js
+++ server/api/generate-pdf.js
@@ -1,20 +1,33 @@
 import PDFTemplate from'../documents/index.js';
-import pdf from 'html-pdf-node';
+// import pdf from 'html-pdf-node';
+import puppeteer from 'puppeteer';
+
 export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
      return res.status(200).end();
-    
   }
-console.log('Received request to generate PDF:', req.method, req.body);
   if (req.method !== 'POST') {
     return res.status(405).send('Method Not Allowed');
   }
 
   try {
     const html = PDFTemplate(req.body); // Should return full HTML string
-    const file = { content: html };
-    const options = { format: 'A4' };
-    const pdfBuffer =await pdf.generatePdf(file, options);
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+await page.setViewport({width: 1080, height: 1024});
+
+await page.setContent(html)
+
+    const pdfBuffer =await page.pdf({
+  path: 'test.pdf',
+  format: 'A4',
+  margin: {
+        top: "20px",
+        left: "20px",
+        right: "20px",
+        bottom: "20px"
+  }    
+});
      res.setHeader('Content-Type', 'application/pdf');
     res.setHeader('Content-Disposition', 'attachment; filename=payslip.pdf');
     return res.status(200).send(pdfBuffer); // Send the PDF buffer as response
